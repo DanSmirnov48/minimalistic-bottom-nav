c19feeaaa2b893f5487995d78e38d436b50d3856
Dan <daniel.smirnov17@gmail.com>
2024-05-14 23:13:59 +0100

    added a test navbar to test item transition of active state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
+import { SetStateAction, useState } from "react";
 import { MainNav } from "./components/main-nav"
+import { cn } from "./lib/utils";
+import { Icons } from "./components/icons";
 
 export default function Home() {
+  const [activeLink, setActiveLink] = useState('link1');
+
+  const handleLinkClick = (link: SetStateAction<string>) => {
+    setActiveLink(link);
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen pt-28">
       <MainNav />
+
+      <ul className="absolute top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 flex rounded-lg border-2 overflow-hidden">
+        <li
+          className={cn("list-none w-[45px]", activeLink === 'link1' && 'active')}
+          onClick={() => handleLinkClick('link1')}
+        >
+          <a className="block py-3.5 px-2 text-center transition-all duration-500 no-underline" href="#">
+            <Icons.home className="w-5 h-5 m-auto text-muted-foreground" />
+          </a>
+        </li>
+        <li
+          className={cn("list-none w-[45px]", activeLink === 'link2' && 'active')}
+          onClick={() => handleLinkClick('link2')}
+        >
+          <a className="block py-3.5 px-2 text-center transition-all duration-500 no-underline" href="#">
+            <Icons.about className="w-5 h-5 m-auto text-muted-foreground" />
+          </a>
+        </li>
+        <li
+          className={cn("list-none w-[45px]", activeLink === 'link3' && 'active')}
+          onClick={() => handleLinkClick('link3')}
+        >
+          <a className="block py-3.5 px-2 text-center transition-all duration-500 no-underline" href="#">
+            <Icons.mail className="w-5 h-5 m-auto text-muted-foreground" />
+          </a>
+        </li>
+        <li className="slide"></li>
+      </ul>
     </div>
   )
 }
--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,30 +1,32 @@
 import { cn } from "@/lib/utils"
 import { NavItem } from "@/types";
-import { Link } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
 import { ChevronLeftIcon } from "lucide-react";
 import { Icons } from "@/components/icons";
 
 export function MainNav() {
   return (
     <header className="bg-background sticky top-0 z-40 w-fit border-2 rounded-lg">
-      <div className="container flex h-14 items-center space-x-4 sm:justify-center sm:space-x-0">
+      <div className="container p-1 flex h-12 items-center space-x-4 sm:justify-center sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
-          <nav className="flex gap-6">
+          <nav className="flex gap-2">
             {mainNav.map((item, index) => {
               const Icon = item.icon ? Icons[item.icon] : ChevronLeftIcon;
-
               return item.to ? (
-                <Link
+                <NavLink
                   key={index}
                   to={item.to}
-                  className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
-                    item.disabled && "cursor-not-allowed opacity-80"
-                  )}
+                  className={({ isActive }) =>
+                    cn(
+                      "flex items-center text-sm font-medium text-muted-foreground p-2",
+                      item.disabled && "cursor-not-allowed opacity-80",
+                      isActive && "bg-foreground/10 rounded-md"
+                    )
+                  }
                 >
-                  <Icon className="mr-2 h-4 w-4" aria-hidden="true" />
-                  <span>{item.title}</span>
-                </Link>
+                  <Icon className="h-5 w-5" aria-hidden="true" />
+                  {/* <span>{item.title}</span> */}
+                </NavLink>
               ) : (
                 <span
                   key={index}
@@ -32,7 +34,7 @@ export function MainNav() {
                 >
                   {item.title}
                 </span>
-              )
+              );
             })}
           </nav>
         </div>
